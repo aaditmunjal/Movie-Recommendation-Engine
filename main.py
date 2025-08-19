72b46fd8cac5df7eb6f254ab1c96e0c7f92a54d3
aaditmunjal <aaditmunjal@gmail.com>
2025-08-19 06:14:05 -0500

    Make pd dataframes

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from surprise import accuracy, Dataset, SVD, Reader
-from surprise.model_selection import train_test_split
+from surprise import Dataset, SVD, Reader
+import pandas as pd
 
+# Make dataframes for ratings and movies
+ratings_df = pd.read_csv(r'data\ratings.csv', usecols=[0, 1, 2], names=["userId", "movieId", "rating"])
+movies_df = pd.read_csv(r"data\movies.csv")
 
 # path to dataset file
 file_path = r"data\ratings.csv"
@@ -10,11 +13,14 @@ reader = Reader(line_format='user item rating', sep=',')
 
 data = Dataset.load_from_file(file_path, reader=reader)
 
-trainset, testset = train_test_split(data, test_size=0.25)
+# Train on full dataset
+trainset = data.build_full_trainset()
 
 # We'll use the famous SVD algorithm.
 algo = SVD()
-
 algo.fit(trainset)
 
-print(algo.estimate(1, 2))
+
+# TODO: given userId, recommend best movies
+def make_recs(userId):
+    print(algo.predict("1", "2"))
