e9e8c64dfb41794cb07a71be03962af0485b5ba7
aaditmunjal <aaditmunjal@gmail.com>
2025-08-20 10:23:30 -0500

    optimized make_recs

--- main.py
+++ main.py
@@ -22,26 +22,22 @@ algo.fit(trainset)
 
 
 # Make recommendations for the userId
-def make_recs(userId):
-    # Get movies that have already been rated by the user
-    ratedByUser = ratings_df[ratings_df['userId'] == userId]['movieId'].to_list()
-    
-    # Get movies that have not been rated by the user yet
-    possibleRecs = movies_df[~movies_df['movieId'].isin(ratedByUser)].reset_index(drop=True)
-    # Get a list of the movieIds of unrated movies
-    recList = possibleRecs['movieId'].to_list()
-    
-    # Predict ratings for all unrated movies
-    out = []
-    for i in recList:
-        out.append((algo.predict(str(userId), str(i))).est)
+def make_recs(userId, top_n=10):
+    # Movies already rated by the user
+    rated_by_user = set(ratings_df.loc[ratings_df['userId'] == userId, 'movieId'])
 
-    possibleRecs['estimate'] = out
+    # Filter out rated movies
+    possible_recs = movies_df.loc[~movies_df['movieId'].isin(rated_by_user)].copy()
 
-    # Recommend the top 10
-    top_10 = possibleRecs.nlargest(10, 'estimate')['title'].to_list()
-    
-    print("Here are some recommendations: ")
-    for i in top_10:
-        print(i)
+    # Predict ratings for unrated movies (list comprehension)
+    possible_recs['estimate'] = [
+        algo.predict(str(userId), str(movie_id)).est 
+        for movie_id in possible_recs['movieId']
+    ]
+
+    # Select top N recommendations
+    top_movies = possible_recs.nlargest(top_n, 'estimate')['title']
+
+    print("Here are some recommendations:")
+    print("\n".join(top_movies))
 
